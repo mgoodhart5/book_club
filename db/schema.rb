654d6ebfa3cd0b75082f8a598ecce5939a8b0309
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-05 14:22:45 -0700

    Creates reviews migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181205210957) do
+ActiveRecord::Schema.define(version: 20181205212157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 20181205210957) do
     t.string "title"
     t.integer "pages"
     t.integer "publishing_year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "title"
+    t.integer "rating"
+    t.text "review_text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
