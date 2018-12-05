1d1c3d91feb02d45ab4df677a2b1b927da79c7c6
Mary Goodhart <marya.grace5@gmail.com>
2018-12-05 16:18:44 -0700

    adds feature test for new book form

--- spec/features/user_can_add_a_new_book_spec.rb
+++ spec/features/user_can_add_a_new_book_spec.rb
@@ -9,4 +9,23 @@ describe 'as a visitor to the book index page' do
 
     expect(current_path).to eq(new_book_path)
   end
+
+  it 'should let fill out a form and create a new book' do
+    book_title = "New Book"
+    book_author = "New Author"
+    book_pages = 100
+    book_publishing_year = 2000
+
+    visit new_book_path
+
+    fill_in :book_title, with: book_title
+    fill_in :book_author, with: book_author
+    fill_in :book_pages, with: book_pages
+    fill_in :book_publishing_year, with: book_publishing_year
+
+    click_button "Create Book"
+
+    expect(current_path).to eq(book_path(Book.last.id))
+    expect(page).to have_content(book_title)    
+  end
 end
