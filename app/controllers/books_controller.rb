5b40e6abe78df72e88fa01e026ea516b9c316158
Mary Goodhart <marya.grace5@gmail.com>
2018-12-05 16:42:19 -0700

    books controller adds, moving forward with new book feature test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,22 @@ class BooksController < ApplicationController
   end
 
   def new
-    
+    @book = Book.new
+  end
+
+  def create
+    author = Author.create(name: author_params[:authors])
+    book = author.books.create(book_params)
+    redirect_to "/books/#{book.id}"
+  end
+
+  private
+
+  def author_params
+    params.require(:book).permit(:authors)
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :pages, :publishing_year)
   end
 end
--- spec/features/user_can_add_a_new_book_spec.rb
+++ spec/features/user_can_add_a_new_book_spec.rb
@@ -12,20 +12,20 @@ describe 'as a visitor to the book index page' do
 
   it 'should let fill out a form and create a new book' do
     book_title = "New Book"
-    book_author = "New Author"
+    book_authors = "New Author"
     book_pages = 100
     book_publishing_year = 2000
 
     visit new_book_path
 
     fill_in :book_title, with: book_title
-    fill_in :book_author, with: book_author
+    fill_in :book_authors, with: book_authors
     fill_in :book_pages, with: book_pages
     fill_in :book_publishing_year, with: book_publishing_year
 
     click_button "Create Book"
 
     expect(current_path).to eq(book_path(Book.last.id))
-    expect(page).to have_content(book_title)    
+    expect(page).to have_content(book_title)
   end
 end
