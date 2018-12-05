5d920d8431d2783b4a00655e2d6b31894bdcf2b3
Mary Goodhart <marya.grace5@gmail.com>
2018-12-04 17:02:56 -0700

    adds feature test to view authors

--- spec/features/user_sees_book_index_spec.rb
+++ spec/features/user_sees_book_index_spec.rb
@@ -7,12 +7,27 @@ describe 'as a visitor to the app' do
 
     visit '/books'
 
-
     expect(page).to have_content(book_1.title)
     expect(page).to have_content("Pages: #{book_1.pages}")
     expect(page).to have_content("Published in: #{book_1.publishing_year}")
     expect(page).to have_content(book_2.title)
     expect(page).to have_content("Pages: #{book_2.pages}")
     expect(page).to have_content("Published in: #{book_2.publishing_year}")
+  end
+
+  it 'should show each books authors' do
+    book_1 = Book.create(title: "book_1", pages: 1, publishing_year: 2001)
+    book_2 = Book.create(title: "book_2", pages: 2, publishing_year: 2002)
+    author_1 = Author.create(name: "author_1")
+    author_2 = Author.create(name: "author_2")
+
+    visit '/books'
+
+    within("book-#{book_1.id}") do
+      expect(page).to have_content(author_1.name)
+    end
+    within("book-#{book_2.id}") do
+      expect(page).to have_content(author_2.name)
+    end
   end
 end
