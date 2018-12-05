8ff93cb1e2697979923be7dc8b806a098f53e828
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-05 15:36:48 -0700

    Adds model test to Book to validate review relationship

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -10,5 +10,6 @@ RSpec.describe Book, type: :model do
   describe 'relationships' do
     it {should have_many(:book_authors)}
     it {should have_many(:authors).through(:book_authors)}
+    it {should have_many(:reviews)}
   end
 end
