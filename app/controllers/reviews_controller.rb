c24cd80bce91ab865492f72d9ff1d4bc09097831
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-06 15:38:29 -0700

    Adds review creation logic and username titlecase feature test

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,6 +6,22 @@ class ReviewsController < ApplicationController
   end
   
   def create
+    book = Book.find(params[:book_id])
+    user = User.create(user_params)
+    review = user.reviews.create(review_params)
+    book.reviews << review
     
+    redirect_to book_path(book)
+  end
+  
+  private
+  
+  def user_params
+    initial_user_params = params.require(:review).permit(:user)
+    {name: initial_user_params[:user]}
+  end
+  
+  def review_params
+    params.require(:review).permit(:title, :rating, :review_text)
   end
 end
--- spec/features/user_can_add_new_review_spec.rb
+++ spec/features/user_can_add_new_review_spec.rb
@@ -36,4 +36,31 @@ describe 'When a user visits a book show page' do
       expect(page).to have_content(review_title)
     end
   end
+  
+  it 'should ensure user name is in title case' do
+    book = Book.create(title: "Book 1", pages: 1, publishing_year: 2001)
+    review_title = "Enjoyed it"
+    user_name = "me too"
+    user_name_titlecase = "Me Too"
+    rating = 5
+    review_text = "Fabulous book loved every minute"
+    
+    visit new_book_review_path(book)
+    
+    fill_in :review_title, with: review_title
+    fill_in :review_user, with: user_name
+    fill_in :review_rating, with: rating
+    fill_in :review_review_text, with: review_text
+    click_button "Create Review"
+    
+    expect(current_path).to eq(book_path(book))
+    expect(User.last.name).to eq(user_name_titlecase)
+    
+    within "#review-#{Review.last.id}" do
+      expect(page).to have_content(user_name_titlecase)
+      expect(page).to have_content(review_text)
+      expect(page).to have_content(rating)
+      expect(page).to have_content(review_title)
+    end
+  end
 end
