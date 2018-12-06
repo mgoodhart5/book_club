8002a3674598c3e14c1cb31b4a9f873b601c6279
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-06 14:56:22 -0700

    Adds new review feature test

--- spec/features/user_can_add_new_review_spec.rb
+++ spec/features/user_can_add_new_review_spec.rb
@@ -10,4 +10,30 @@ describe 'When a user visits a book show page' do
     
     expect(current_path).to eq(new_book_review_path(book))
   end
+  
+  it 'can create a new review' do
+    book = Book.create(title: "Book 1", pages: 1, publishing_year: 2001)
+    review_title = "Enjoyed it"
+    user_name = "Me"
+    rating = 5
+    review_text = "Fabulous book loved every minute"
+    
+    visit new_book_review_path(book)
+    
+    fill_in :review_title, with: review_title
+    fill_in :review_username, with: user_name
+    fill_in :review_rating, with: rating
+    fill_in :review_text, with: review_text
+    click_button "Create Review"
+    
+    expect(current_path).to eq(book_path(book))
+    expect(User.last.name).to eq(user_name)
+    
+    within "#review-#{Review.last.id}" do
+      expect(page).to have_content(user_name)
+      expect(page).to have_content(review_text)
+      expect(page).to have_content(rating)
+      expect(page).to have_content(review_title)
+    end
+  end
 end
