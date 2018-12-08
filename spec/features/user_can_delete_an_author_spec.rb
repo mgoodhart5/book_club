fc475db120f375722b465b054cdf5e692b622d2e
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-08 15:44:50 -0700

    Adds test for making sure co-authors aren't deleted

--- spec/features/user_can_delete_an_author_spec.rb
+++ spec/features/user_can_delete_an_author_spec.rb
@@ -27,4 +27,22 @@ describe 'When a user visits an author show page' do
     expect(page).to_not have_content(book.title)
     expect(page).to_not have_content(book_2.title)
   end
+  it 'should delete its own co-authored books but not the co-author' do
+    author = Author.create(name: "author_1")
+    author_2 = Author.create(name: "Author 2")
+    book = author.books.create(title: "Book 1", pages: 1, publishing_year: 2001)
+    book_2 = Book.create(authors: [author, author_2], title: "Book 2", pages: 1, publishing_year: 2001)
+    book_3 = author_2.books.create(title: "Book 3", pages: 1, publishing_year: 2001)
+    
+    visit author_path(author) 
+    
+    click_link "Delete Author" 
+    
+    expect(current_path).to eq(books_path)
+    expect(page).to_not have_content(author.name)
+    expect(page).to_not have_content(book.title)
+    expect(page).to_not have_content(book_2.title)
+    expect(page).to have_content(author_2.name)
+    expect(page).to have_content(book_3.title)
+  end
 end
