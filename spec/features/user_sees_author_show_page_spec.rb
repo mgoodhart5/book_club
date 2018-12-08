49b9527f83818b71cefaa2c849699e3fb628e152
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-08 14:23:53 -0700

    Adds author show page and routes

--- spec/features/user_sees_author_show_page_spec.rb
+++ spec/features/user_sees_author_show_page_spec.rb
@@ -17,15 +17,15 @@ describe 'When a user visits an author show page' do
     visit author_path(author)
     
     expect(page).to have_content(author.name)
-    within "book-#{book_1.id}" do
+    within "#book-#{book_1.id}" do
       expect(page).to have_content(book_1.title)
       expect(page). to have_content("Pages: #{book_1.pages}")
-      expect(page). to have_content("Published in: #{book_1.pages}")
+      expect(page). to have_content("Published in: #{book_1.publishing_year}")
     end
-    within "book-#{book_2.id}" do
+    within "#book-#{book_2.id}" do
       expect(page).to have_content(book_2.title)
       expect(page). to have_content("Pages: #{book_2.pages}")
-      expect(page). to have_content("Published in: #{book_2.pages}")
+      expect(page). to have_content("Published in: #{book_2.publishing_year}")
     end
   end
 end
