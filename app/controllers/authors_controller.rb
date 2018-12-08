dbab4409b6a3f67b3259773b0540df1fad77a598
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-08 15:37:39 -0700

    Adds destroy ability to Author

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,4 +2,14 @@ class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
   end
+  
+  def destroy
+    author = Author.find(params[:id])
+    books = author.books
+    books.each do |book|
+      Book.destroy(book.id)
+    end
+    Author.destroy(params[:id])
+    redirect_to books_path
+  end
 end
