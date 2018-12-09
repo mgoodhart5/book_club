51d810bff34bb076004a2dffab5674b053205b35
Mary Goodhart <marya.grace5@gmail.com>
2018-12-08 19:43:48 -0700

    added new drop down box, fixed styling for new review still figuring out how to test drop downs

--- spec/features/user_can_add_new_review_spec.rb
+++ spec/features/user_can_add_new_review_spec.rb
@@ -3,40 +3,40 @@ require 'rails_helper'
 describe 'When a user visits a book show page' do
   it 'can go to new review page' do
     book = Book.create(title: "Book 1", pages: 1, publishing_year: 2001)
-    
+
     visit book_path(book)
-    
-    click_on 'New Review' 
-    
+
+    click_on 'New Review'
+
     expect(current_path).to eq(new_book_review_path(book))
   end
-  
+
   it 'can create a new review' do
     book = Book.create(title: "Book 1", pages: 1, publishing_year: 2001)
     review_title = "Enjoyed it"
     user_name = "Me"
-    rating = 5
+    # rating = 5
     review_text = "Fabulous book loved every minute"
-    
+
     visit new_book_review_path(book)
-    
+
     fill_in :review_title, with: review_title
     fill_in :review_user, with: user_name
-    fill_in :review_rating, with: rating
+    select('5', :from => 'rating')
     fill_in :review_review_text, with: review_text
     click_button "Create Review"
-    
+
     expect(current_path).to eq(book_path(book))
     expect(User.last.name).to eq(user_name)
-    
+
     within "#review-#{Review.last.id}" do
       expect(page).to have_content(user_name)
       expect(page).to have_content(review_text)
-      expect(page).to have_content(rating)
+      expect(page).to have_select('my-select', selected: 'Option 2')
       expect(page).to have_content(review_title)
     end
   end
-  
+
   it 'should ensure user name is in title case' do
     book = Book.create(title: "Book 1", pages: 1, publishing_year: 2001)
     review_title = "Enjoyed it"
@@ -44,18 +44,18 @@ describe 'When a user visits a book show page' do
     user_name_titlecase = "Me Too"
     rating = 5
     review_text = "Fabulous book loved every minute"
-    
+
     visit new_book_review_path(book)
-    
+
     fill_in :review_title, with: review_title
     fill_in :review_user, with: user_name
     fill_in :review_rating, with: rating
     fill_in :review_review_text, with: review_text
     click_button "Create Review"
-    
+
     expect(current_path).to eq(book_path(book))
     expect(User.last.name).to eq(user_name_titlecase)
-    
+
     within "#review-#{Review.last.id}" do
       expect(page).to have_content(user_name_titlecase)
       expect(page).to have_content(review_text)
@@ -63,7 +63,7 @@ describe 'When a user visits a book show page' do
       expect(page).to have_content(review_title)
     end
   end
-  
+
   it 'should ensure user name is unique' do
     book = Book.create(title: "Book 1", pages: 1, publishing_year: 2001)
     user = User.create(name: "Me Too")
@@ -71,23 +71,40 @@ describe 'When a user visits a book show page' do
     user_name = "Me Too"
     rating = 5
     review_text = "Fabulous book loved every minute"
-    
+
     visit new_book_review_path(book)
-    
+
     fill_in :review_title, with: review_title
     fill_in :review_user, with: user_name
     fill_in :review_rating, with: rating
     fill_in :review_review_text, with: review_text
     click_button "Create Review"
-    
+
     expect(current_path).to eq(book_path(book))
     expect(user.id).to eq(User.last.id)
-    
+
     within "#review-#{Review.last.id}" do
       expect(page).to have_content(user_name)
       expect(page).to have_content(review_text)
       expect(page).to have_content(rating)
       expect(page).to have_content(review_title)
     end
+  end
+  it 'cannot rate higher than 5' do
+    book = Book.create(title: "Book 1", pages: 1, publishing_year: 2001)
+    review_title = "Enjoyed it"
+    user_name = "Me"
+    rating = 7
+    review_text = "Fabulous book loved every minute"
+
+    visit new_book_review_path(book)
+
+    fill_in :review_title, with: review_title
+    fill_in :review_user, with: user_name
+    fill_in :review_rating, with: rating
+    fill_in :review_review_text, with: review_text
+    click_button "Create Review"
+
+    expect(current_path).to eq(new_book_review_path(book))
   end
 end
