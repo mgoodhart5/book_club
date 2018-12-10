3953cb07a4f2d3a88f774b7a7308ec11bf24716b
Mary Goodhart <marya.grace5@gmail.com>
2018-12-10 15:38:07 -0700

    adds test for top user reviewers

--- spec/features/user_sees_book_index_spec.rb
+++ spec/features/user_sees_book_index_spec.rb
@@ -135,4 +135,32 @@ describe 'as a visitor to the app' do
       expect(page).to_not have_content(book_3.title)
     end
   end
+  it "should see an area showing statistics of the three users with the most reviews" do
+    book_1 = Book.create(title: "Book 11", pages: 1, publishing_year: 2001)
+    book_2 = Book.create(title: "Book 22", pages: 2, publishing_year: 2002)
+    book_3 = Book.create(title: "Book 33", pages: 3, publishing_year: 2003)
+    book_4 = Book.create(title: "Book 44", pages: 4, publishing_year: 2004)
+    user_1 = User.create(name: "Steve")
+    user_2 = User.create(name: "Brenna")
+    user_3 = User.create(name: "Leigh")
+    user_4 = User.create(name: "Logan")
+    book_1.reviews.create(title: "Review 1", rating: 2, review_text: "Review 1 here", user: user_3)
+    book_2.reviews.create(title: "Review 2", rating: 4, review_text: "Review 2 here", user: user_3)
+    book_3.reviews.create(title: "Review 3", rating: 5, review_text: "Review 3 here", user: user_2)
+    book_4.reviews.create(title: "Review 4", rating: 3, review_text: "Review 4 here", user: user_2)
+    book_1.reviews.create(title: "Review 11", rating: 2, review_text: "Review 11 here", user: user_2)
+    book_2.reviews.create(title: "Review 22", rating: 4, review_text: "Review 22 here", user: user_1)
+    book_3.reviews.create(title: "Review 33", rating: 5, review_text: "Review 3 here", user: user_3)
+    book_4.reviews.create(title: "Review 44", rating: 3, review_text: "Review 4 here", user: user_4)
+    book_1.reviews.create(title: "Review 111", rating: 2, review_text: "Review 11 here", user: user_4)
+
+    visit books_path
+
+    within "#stats-users" do
+      expect(page).to have_content(user_3.name)
+      expect(page).to have_content(user_4.name)
+      expect(page).to have_content(user_2.name)
+      expect(page).to_not have_content(user_1.name)
+    end
+  end
 end
