21571992c875dbd583ac9e9646a71e8e291f5146
Anna Smolentzov <annasmolentzov@gmail.com>
2018-12-10 11:16:47 -0700

    Adds root routing and welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :reviews, shallow: true, only: [:new, :create, :destroy]
   end
 
+  root to: 'welcome#index'
   resources :users, only: [:show]
 
   resources :authors, only: [:show, :destroy]
