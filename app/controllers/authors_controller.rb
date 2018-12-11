0a7e39ea216fde71a3a4dd28d888f15854a41069
Mary Goodhart <marya.grace5@gmail.com>
2018-12-11 15:56:18 -0700

    refactors author destroy method

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,14 +1,10 @@
-class AuthorsController < ApplicationController 
+class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
   end
-  
+
   def destroy
-    author = Author.find(params[:id])
-    books = author.books
-    books.each do |book|
-      Book.destroy(book.id)
-    end
+    Author.find(params[:id])
     Author.destroy(params[:id])
     redirect_to books_path
   end
