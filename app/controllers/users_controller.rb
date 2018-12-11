fd0edf2eeab778c34b8fbaf118bf962f00041e06
Mary Goodhart <marya.grace5@gmail.com>
2018-12-10 18:28:21 -0700

    adds test and implements code for user show page asc chronological order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    if params[:rating] == 'asc'
+      @reviews = @user.sort_reviews_chronologically
+    else
+      @reviews = @user.reviews
+    end
   end
-
 end
